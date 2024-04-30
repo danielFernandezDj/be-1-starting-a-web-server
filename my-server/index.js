5a6b9c3b1b7283505ba485001bca5f2df8716885
danielFernandezJd <mdanif9728@gmail.com>
2024-04-29 21:49:09 -0700

    delete the old code

--- my-server/index.js
+++ my-server/index.js
@@ -1,12 +1,12 @@
 // This is first to create the server 
 let http = require('http')
 let server = http.createServer(function (req, res) {
-  res.write("<h1 style='color: blue'>Hello FSD!</h1>")
+  res.writeHeader(206)
+  res.write('<h1>Bark!</h1><img src="https://httpstatusdogs.com/img/206.jpg" alt="206">')
   res.end()
 })
 
 // Create the port number
-server.listen(3000, function() {
-    console.log('I am awake!')
+server.listen(3000, function () {
+  console.log('I am awake!')
 })
-
